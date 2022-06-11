6cc874cf57499afce6235394fb56bd8b1072aac4
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-11 08:23:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+function() {
+    var customerName = bob;
+    console.log(window.customerName); // bob
+}
+
+const myVar = customerName;
+
+function myFunc() {
+    const customerName = bob;
+
+    return myVar;
+}
+
+myFunc();
+// => BOB
+
+
+function bestCustomer() {
+    (console.log(window.bestCustomer); // undefined
+
+        bestCustomer();
+
+        console.log(window.bestCustomer); // 'not bob'
+
+    };
+
+    const overwriteBestCustomer = "bob";
+
+    function bestCustomer() {
+        const bestCustomer = "bob";
+
+        console.log(window.bestCustomer); // bob
+
+    });
+
+const LeastFavoriteCustomer = "bib";
+
+function changeLeastFavoriteCustomer() {
+    console.log(window.changeLeastFavoriteCustomer); // bib
+
+}
